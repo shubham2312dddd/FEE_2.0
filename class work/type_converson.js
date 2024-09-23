5cdf82b0a40ea1a804533c01f96cb8f8d1c8fcb1
shubham2312dddd <150227484+shubham2312dddd@users.noreply.github.com>
2024-09-23 09:10:31 +0530

    Add files via upload

---
+++ class work/type_converson.js
@@ -0,0 +1,16 @@
+console.log(1 + 2);
+console.log(1+"2");
+console.log(5-"2");
+console.log(10 + "A");
+console.log(20 - "A");
+console.log(10 + "2" + "A");
+console.log(10 - "0" + "1");
+
+// string to integer
+
+let a = "20";
+console.log(typeof a);
+let c = Number(a);
+console.log(typeof c);
+let d = parseInt(a);
+console.log(typeof d);
