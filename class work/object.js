91008132421cc892300ff09fc1f34e9cf6419da2
shubham2312dddd <150227484+shubham2312dddd@users.noreply.github.com>
2024-09-23 09:11:36 +0530

    Add files via upload

---
+++ class work/object.js
@@ -0,0 +1,69 @@
+// const obj = new Object();
+// obj.name = "Sidharth";
+// console.log(obj);
+// console.log(typeof obj);
+
+// ----------Literals--------------
+
+// const users={
+//     name:"Shubhi",
+//     name:"Shubhi Yadav",
+//     age:25,
+//     subject:'CSE',
+//     cgpa:8.1
+// }
+// users.name = "Mohit";
+// console.log(users);
+// console.log(users['FullName']);
+// console.log(users ['age']);
+// console.log(typeof users);
+
+// const users={
+//     user1:{
+//         name1:"Shubhi",
+//         age:25,
+//     },
+//     user2:{
+//         name2:"Sehaj",
+//         age:26,
+//     },
+//     user3:{
+//         name3:{
+//             name11:"Sidharth",
+//         },
+//         // name:"Sidharth",
+//         age:27,
+//     }
+// }
+// console.log(users.user2.name2);
+// console.log(users.user3.name3.name11);
+
+
+// const arr=[
+//     {
+//      name:"shubhi",
+//      age:12
+//     },
+//     {
+//         name:"shubhank",
+//         age:24
+//     },
+//     {
+//         name:"sid",
+//         age:10
+//     },
+//     {
+//         name:"sehaj",
+//         age:18
+//     }
+// ]
+// console.log(arr);
+
+
+// let obj1 = {name1:"Shubham",age:25}
+// let obj2 = {name2:"Shubham",age1:24}
+// let obj3 = {obj1,obj2};
+// let obj3 = {...obj1,...obj2};
+// console.log(obj3);
+// let obj4 = Object.assign(obj1,obj2)
+// console.log(obj4);
